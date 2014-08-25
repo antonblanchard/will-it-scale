72d7bcde3d37d422134b85486f9b8cdedcd53eb0
Anton Blanchard <anton@samba.org>
2014-08-25 20:32:58 +1000

    Add seek+read and seek+write to same file testcases
    We had seek+read and seek+write to different files but not
    this one.
    
    Signed-off-by: Anton Blanchard <anton@samba.org>

---
+++ tests/writeseek2.c
@@ -0,0 +1,42 @@
+#include <fcntl.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <string.h>
+#include <assert.h>
+
+#define BUFLEN 4096
+
+static char tmpfile[] = "/tmp/willitscale.XXXXXX";
+
+char *testcase_description = "Same file seek+read";
+
+void testcase_prepare(void)
+{
+	char buf[BUFLEN];
+	int fd = mkstemp(tmpfile);
+
+	memset(buf, 0, sizeof(buf));
+	assert(fd >= 0);
+	assert(write(fd, buf, sizeof(buf)) == sizeof(buf));
+	close(fd);
+}
+
+void testcase(unsigned long long *iterations)
+{
+	char buf[BUFLEN];
+	int fd = open(tmpfile, O_RDWR);
+
+	assert(fd >= 0);
+
+	while (1) {
+		lseek(fd, 0, SEEK_SET);
+		assert(write(fd, buf, BUFLEN) == BUFLEN);
+
+		(*iterations)++;
+	}
+}
+
+void testcase_cleanup(void)
+{
+	unlink(tmpfile);
+}
