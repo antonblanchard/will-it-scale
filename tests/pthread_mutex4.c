486b5b2271c9c391026a95bf433f90b13783c29f
Tommi Rantala <tommi.t.rantala@nokia.com>
2020-02-12 11:36:29 +0200

    pthread_mutex4: Workaround compiler optimization
    GCC eliminates the local variable update, making the pthread_mutex4
    testcase in practice equal to pthread_mutex1 (which does mutex
    lock+unlock only).
    
    Trick the compiler by allocating the local variable with alloca(), it's
    enough to "workaround" the optimization. (Making the variable "volatile"
    works too, but adds more machine instructions.)
    
    Now we see the stack variable update in the disassembly (x86_64):
    
      $ objdump -d pthread_mutex4_processes |grep "<testcase>" -A25
      0000000000401bd0 <testcase>:
        401bd0:       55                      push   %rbp
        401bd1:       48 89 e5                mov    %rsp,%rbp
        401bd4:       41 54                   push   %r12
        401bd6:       49 89 fc                mov    %rdi,%r12
        401bd9:       53                      push   %rbx
        401bda:       48 83 ec 10             sub    $0x10,%rsp
        401bde:       48 8d 5c 24 0f          lea    0xf(%rsp),%rbx
        401be3:       48 83 e3 f0             and    $0xfffffffffffffff0,%rbx
        401be7:       48 c7 03 00 00 00 00    movq   $0x0,(%rbx)
        401bee:       66 90                   xchg   %ax,%ax
        401bf0:       bf a0 71 40 00          mov    $0x4071a0,%edi
        401bf5:       e8 76 f6 ff ff          callq  401270 <pthread_mutex_lock@plt>
    ->  401bfa:       48 83 03 01             addq   $0x1,(%rbx)
        401bfe:       bf a0 71 40 00          mov    $0x4071a0,%edi
        401c03:       e8 a8 f5 ff ff          callq  4011b0 <pthread_mutex_unlock@plt>
        401c08:       49 83 04 24 01          addq   $0x1,(%r12)
        401c0d:       eb e1                   jmp    401bf0 <testcase+0x20>
        401c0f:       90                      nop

--- tests/pthread_mutex4.c
+++ tests/pthread_mutex4.c
@@ -1,5 +1,6 @@
 #define _GNU_SOURCE
 #include <pthread.h>
+#include <alloca.h>
 
 char *testcase_description = "Contended pthread mutex with local update";
 
@@ -7,11 +8,13 @@ pthread_mutex_t mutex = PTHREAD_MUTEX_INITIALIZER;
 
 void testcase(unsigned long long *iterations, unsigned long nr)
 {
-	unsigned long val;
+	// alloca() because plain "unsigned long val" gets optimized away.
+	unsigned long *val = alloca(sizeof(unsigned long));
+	*(val) = 0;
 
 	while (1) {
 		pthread_mutex_lock(&mutex);
-		val++;
+		(*val)++;
 		pthread_mutex_unlock(&mutex);
 
 		(*iterations)++;
