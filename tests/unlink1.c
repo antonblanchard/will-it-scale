8daa3bce39e382c99ec27b6b087f5c549413b6bd
Dust Li <dust.li@linux.alibaba.com>
2020-05-14 14:42:45 +0800

    unlink1: fix tmpfile leak after exit
    unlink1.c forget to cleanup the tmpfile created by it's child.
    Since the parent will kill the child thread using pthread_cancel()
    or kill. The child has no chance to cleanup it's tmpfiles.
    
    We move the tmpfiles creating & removing stage into testcase_prepare()
    & testcase_cleanup(), which will run in the master thread/process
    which can make sure all the tmpfiles will be deleted after the test.
    
    Signed-off-by: Dust Li <dust.li@linux.alibaba.com>

--- tests/unlink1.c
+++ tests/unlink1.c
@@ -4,26 +4,55 @@
 #include <sys/stat.h>
 #include <fcntl.h>
 #include <assert.h>
+#include <string.h>
 
 char *testcase_description = "Separate file open/close/unlink";
 
+#define template 	"/tmp/willitscale.XXXXXX"
+static char (*tmpfiles)[sizeof(template)];
+static int local_nr_tasks;
+
+
+void testcase_prepare(unsigned long nr_tasks)
+{
+	int i;
+	tmpfiles = (char(*)[sizeof(template)])malloc(sizeof(template) * nr_tasks);
+	assert(tmpfiles);
+
+	for (i = 0; i < nr_tasks; i++) {
+		strcpy(tmpfiles[i], template);
+		char *tmpfile = tmpfiles[i];
+		int fd = mkstemp(tmpfile);
+
+		assert(fd >= 0);
+		close(fd);
+		unlink(tmpfile);
+	}
+
+	local_nr_tasks = nr_tasks;
+}
+
+
 void testcase(unsigned long long *iterations, unsigned long nr)
 {
-	char tmpfile[] = "/tmp/willitscale.XXXXXX";
-	int fd = mkstemp(tmpfile);
-
-	assert(fd >= 0);
-	close(fd);
-	unlink(tmpfile);
+	char *tmpfile = tmpfiles[nr];
 
 	while (1) {
-		fd = open(tmpfile, O_RDWR|O_CREAT, 0600);
+		int fd = open(tmpfile, O_RDWR|O_CREAT, 0600);
 		assert(fd >= 0);
 		close(fd);
 		unlink(tmpfile);
 
 		(*iterations)++;
 	}
+}
 
-	unlink(tmpfile);
+
+void testcase_cleanup(void)
+{
+	int i;
+	for (i = 0; i < local_nr_tasks; i++) {
+		unlink(tmpfiles[i]);
+	}
+	free(tmpfiles);
 }
