39065ba416bd7c788ae7c95ef8c670dcb3be20cf
Dust Li <dust.li@linux.alibaba.com>
2020-06-03 15:47:01 +0800

    open2: fix tmpfile leak after exit
    Same as unlink1, when the child is killed by the parent,
    the /tmp/willitscale-XXXXXX/willitscale is not removed.
    
    By moving the tmpdir and tmpfiles creating & removing stage into
    testcase_prepare() & testcase_cleanup(), which will run in the
    master thread/process, that can make sure all the tmpdirs and tmpfiles
    will be deleted after the test.
    
    Signed-off-by: Dust Li <dust.li@linux.alibaba.com>

--- tests/open2.c
+++ tests/open2.c
@@ -6,20 +6,42 @@
 #include <fcntl.h>
 #include <assert.h>
 #include <limits.h>
+#include <string.h>
 
 char *testcase_description = "Separate file open/close in different directories";
 
-void testcase(unsigned long long *iterations, unsigned long nr)
+#define template 	"/tmp/willitscale.XXXXXX"
+static char (*tmpdirs)[sizeof(template)];
+static unsigned long local_nr_tasks;
+
+void testcase_prepare(unsigned long nr_tasks)
 {
-	char tmpdir[] = "/tmp/willitscale.XXXXXX";
+	int i;
+	tmpdirs = (char(*)[sizeof(template)])malloc(sizeof(template) * nr_tasks);
+	assert(tmpdirs);
+
 	char tmpfile[PATH_MAX];
 	int fd;
 
-	assert(mkdtemp(tmpdir) != NULL);
-	sprintf(tmpfile, "%s/willitscale", tmpdir);
-	fd = open(tmpfile, O_RDWR|O_CREAT, 0600);
-	assert(fd >= 0);
-	close(fd);
+	for (i = 0; i < nr_tasks; i++) {
+		strcpy(tmpdirs[i], template);
+		char *tmpdir = tmpdirs[i];
+		assert(mkdtemp(tmpdir) != NULL);
+		sprintf(tmpfile, "%s/willitscale", tmpdir);
+		fd = open(tmpfile, O_RDWR|O_CREAT, 0600);
+		assert(fd >= 0);
+		close(fd);
+	}
+
+	local_nr_tasks = nr_tasks;
+}
+
+
+void testcase(unsigned long long *iterations, unsigned long nr)
+{
+	int fd;
+	char tmpfile[PATH_MAX];
+	sprintf(tmpfile, "%s/willitscale", tmpdirs[nr]);
 
 	while (1) {
 		fd = open(tmpfile, O_RDWR);
@@ -28,4 +50,18 @@ void testcase(unsigned long long *iterations, unsigned long nr)
 
 		(*iterations)++;
 	}
+}
+
+
+void testcase_cleanup(void)
+{
+	int i;
+	char tmpfile[PATH_MAX];
+
+	for (i = 0; i < local_nr_tasks; i++) {
+		sprintf(tmpfile, "%s/willitscale", tmpdirs[i]);
+		unlink(tmpfile);
+		rmdir(tmpdirs[i]);
+	}
+	free(tmpdirs);
 }
